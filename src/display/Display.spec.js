f602358b0ef2d17ed38188439515ba70f9bdbf66
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-01-30 15:06:34 -0600

    dashboard and display tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, waitForElement } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+describe('<Display/>', () => {
+    it('should display closed gate', () => {
+        const { getByText } = render(<Display closed={true} locked={false} />);
+
+        getByText(/closed/i);
+        getByText(/unlocked/i);
+    })
+})
